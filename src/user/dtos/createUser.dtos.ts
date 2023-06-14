398266f2b7afb9451a3fac25725e5b2fbdb5dc3c
Helton Brito <heltoncarlospa@hotmail.com>
2023-06-13 23:44:51 -0300

    feat: add user controller and create user dtos

---
+++ src/user/dtos/createUser.dtos.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+export interface ICreateUser {
+  email: string;
+  name: string;
+  cpf: string;
+  phone: string;
+  password: string;
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { ICreateUser } from './dtos/createUser.dtos';
 
 @Controller('user')
 export class UserController {
   @Get()
   async getAllUsers() {
     return JSON.stringify({ test: 'abc' });
+  }
+
+  @Post()
+  async createUsers(@Body() createUsers: ICreateUser) {
+    return {
+      ...createUsers,
+      password: undefined,
+    };
   }
 }
